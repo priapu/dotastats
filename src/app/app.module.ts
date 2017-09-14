c5dd6612fe62b9b61ed5175781640f4937ee5b72
Pria Purnama <dudewithmessyhair@gmail.com>
2017-09-15 01:12:07 +0700

    Added myprofile component and added routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { OpendotaService } from './opendota.service';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { MyprofileComponent } from './myprofile/myprofile.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: MyprofileComponent,
+    data: { title: 'My profile' }
+  },
+  {
+    path: 'heroes',
+    component: HeroesListComponent,
+    data: { title: 'Hero list' }
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    MyprofileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [OpendotaService],
   bootstrap: [AppComponent]
--- src/app/opendota.service.ts
+++ src/app/opendota.service.ts
@@ -8,7 +8,11 @@ export class OpendotaService {
   constructor(private http: Http) {}
 
   getHeroes() {
-    return this.http.get('https://api.opendota.com/api/heroes').map((res:Response) => res.json());
+    return this.http.get('https://api.opendota.com/api/heroStats').map((res:Response) => res.json());
+  }
+
+  getProfile(id= '270336628') {
+    return this.http.get('https://api.opendota.com/api/players/'+id).map((res:Response) => res.json());
   }
 
 }
